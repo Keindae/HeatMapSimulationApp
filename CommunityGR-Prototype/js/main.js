e34020c4b3c5456247096da256c090d71236e8f5
Nam Nguyen <namnguyen1995@gmail.com>
2017-10-01 17:48:32 -0400

    Add JSON files for city boundaries

--- CommunityGR-Prototype/js/main.js
+++ CommunityGR-Prototype/js/main.js
@@ -1,3 +1,31 @@
+var map;
+var marker;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map-card'), {
+        center: {lat: 42.955267, lng: -85.671772},
+        zoom: 11
+    });
+
+    marker = new google.maps.Marker({
+        position: {lat: 42.970371, lng: -85.665655},
+        title: "Helen Devos Children's Hospital"
+    });
+
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/wyomingPolygon.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/grandvillePolygon.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/kentwoodPolygon.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/comstockParkPolygon.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/spartaPolygon.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/byronCenter.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/cedarSpringsPolygon.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/rockfordPolygon.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/lowellPolygon.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/eastGrandRapidsPolygon.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/caledoniaPolygon.json');
+    map.data.loadGeoJson('https://raw.githubusercontent.com/nguynam/images/master/grandRapidsPolygon.json');s
+}
+
 function onChangeCheckbox(checkbox){
     if(checkbox.checked){
         marker.setMap(map);

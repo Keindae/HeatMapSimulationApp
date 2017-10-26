8f3df5b855eee9491c4d023f431af5f278f615f7
Nam Nguyen <namnguyen1995@gmail.com>
2017-10-26 15:36:27 -0400

    Add wyoming parks

--- CommunityGR-Prototype/js/parks.js
+++ CommunityGR-Prototype/js/parks.js
@@ -2,7 +2,7 @@ var parks;
 
 function setParks(){
     parks = new google.maps.Data();
-    parks.loadGeoJson('https://raw.githubusercontent.com/Keindae/HeatMapSimulationApp/parks/ParksAndRec/parks.json');
+    parks.loadGeoJson('https://raw.githubusercontent.com/Keindae/HeatMapSimulationApp/parks/ParksAndRec/GrandRapidsParks.json');
     parks.setMap(googleMap);
     parks.setStyle({
         visible: false
